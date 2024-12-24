058dad685e9f10492393722c37d2ca86a6d1e183
DanNachmanyGuesty <dan.nachmany@guesty.com>
2024-12-24 09:10:30 +0200

    Implement Future class

--- src/main/java/bgu/spl/mics/Future.java
+++ src/main/java/bgu/spl/mics/Future.java
@@ -11,55 +11,79 @@ import java.util.concurrent.TimeUnit;
  * No public constructor is allowed except for the empty constructor.
  */
 public class Future<T> {
-	
+
+	private T result;
+	private boolean isDone;
+
 	/**
 	 * This should be the the only public constructor in this class.
 	 */
 	public Future() {
-		//TODO: implement this
+		isDone = false;
+		result = null;
 	}
-	
+
 	/**
-     * retrieves the result the Future object holds if it has been resolved.
-     * This is a blocking method! It waits for the computation in case it has
-     * not been completed.
-     * <p>
-     * @return return the result of type T if it is available, if not wait until it is available.
-     * 	       
-     */
-	public T get() {
-		//TODO: implement this.
-		return null;
+	 * retrieves the result the Future object holds if it has been resolved.
+	 * This is a blocking method! It waits for the computation in case it has
+	 * not been completed.
+	 * <p>
+	 * 
+	 * @return return the result of type T if it is available, if not wait until it
+	 *         is available.
+	 * 
+	 */
+	public synchronized T get() {
+		try {
+			while (!isDone) {
+				wait();
+			}
+			return result;
+		} catch (InterruptedException e) {
+			// ! TODO implement error handling here
+			Thread.currentThread().interrupt(); // Restore the interrupted status
+			return null;
+		}
 	}
-	
+
 	/**
-     * Resolves the result of this Future object.
-     */
-	public void resolve (T result) {
-		//TODO: implement this.
+	 * Resolves the result of this Future object.
+	 */
+	public synchronized void resolve(T result) {
+		this.result = result;
+		this.isDone = true;
+		notifyAll();
 	}
-	
+
 	/**
-     * @return true if this object has been resolved, false otherwise
-     */
-	public boolean isDone() {
-		//TODO: implement this.
-		return false;
+	 * @return true if this object has been resolved, false otherwise
+	 */
+	public synchronized boolean isDone() {
+		return isDone;
 	}
-	
+
 	/**
-     * retrieves the result the Future object holds if it has been resolved,
-     * This method is non-blocking, it has a limited amount of time determined
-     * by {@code timeout}
-     * <p>
-     * @param timout 	the maximal amount of time units to wait for the result.
-     * @param unit		the {@link TimeUnit} time units to wait.
-     * @return return the result of type T if it is available, if not, 
-     * 	       wait for {@code timeout} TimeUnits {@code unit}. If time has
-     *         elapsed, return null.
-     */
-	public T get(long timeout, TimeUnit unit) {
-		//TODO: implement this.
+	 * retrieves the result the Future object holds if it has been resolved,
+	 * This method is non-blocking, it has a limited amount of time determined
+	 * by {@code timeout}
+	 * <p>
+	 * 
+	 * @param timout the maximal amount of time units to wait for the result.
+	 * @param unit   the {@link TimeUnit} time units to wait.
+	 * @return return the result of type T if it is available, if not,
+	 *         wait for {@code timeout} TimeUnits {@code unit}. If time has
+	 *         elapsed, return null.
+	 */
+	public synchronized T get(long timeout, TimeUnit unit) {
+		try {
+			if (!isDone) {
+				unit.timedWait(this, timeout);
+			}
+			return result;
+		} catch (InterruptedException e) {
+			// ! TODO implement error handling here
+			e.printStackTrace();
+		}
 		return null;
 	}
 
