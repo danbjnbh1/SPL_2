c8752e6e3e014128369725392297c8ac530d3b8c
DanNachmanyGuesty <dan.nachmany@guesty.com>
2024-12-27 15:22:57 +0200

    change array to list

--- src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
+++ src/main/java/bgu/spl/mics/application/objects/FusionSlam.java
@@ -1,13 +1,20 @@
 package bgu.spl.mics.application.objects;
 
 /**
- * Manages the fusion of sensor data for simultaneous localization and mapping (SLAM).
- * Combines data from multiple sensors (e.g., LiDAR, camera) to build and update a global map.
- * Implements the Singleton pattern to ensure a single instance of FusionSlam exists.
+ * Manages the fusion of sensor data for simultaneous localization and mapping
+ * (SLAM).
+ * Combines data from multiple sensors (e.g., LiDAR, camera) to build and update
+ * a global map.
+ * Implements the Singleton pattern to ensure a single instance of FusionSlam
+ * exists.
  */
 public class FusionSlam {
     // Singleton instance holder
     private static class FusionSlamHolder {
-        // TODO: Implement singleton instance logic.
+        private static FusionSlam instance = new FusionSlam();
+    }
+
+    public FusionSlam getInstance() {
+        return FusionSlamHolder.instance;
     }
 }
--- src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
+++ src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.List;
+
 /**
  * Represents a group of cloud points corresponding to a specific timestamp.
  * Used by the LiDAR system to store and process point cloud data for tracked objects.
@@ -8,7 +10,7 @@ public class StampedCloudPoints {
     
     private int time;
     private int id;
-    private CloudPoint[] cloudPoints;
+    private List<CloudPoint> cloudPoints;
 
     /**
      * Constructor for StampedCloudPoints.
@@ -16,7 +18,7 @@ public class StampedCloudPoints {
      * @param cloudPoints A 2D array of cloud points.
      * @param time The timestamp of the cloud points.
      */
-    public StampedCloudPoints(CloudPoint[] cloudPoints, int time, int id) {
+    public StampedCloudPoints(List<CloudPoint> cloudPoints, int time, int id) {
         this.cloudPoints = cloudPoints;
         this.time = time;
         this.id = id;
@@ -27,7 +29,7 @@ public class StampedCloudPoints {
      *
      * @return The cloud points.
      */
-    public CloudPoint[] getCloudPoints() {
+    public List<CloudPoint> getCloudPoints() {
         return cloudPoints;
     }
 
@@ -48,7 +50,7 @@ public class StampedCloudPoints {
         this.id = id;
     }   
 
-    public void setCloudPoints(CloudPoint[] cloudPoints) {
+    public void setCloudPoints(List<CloudPoint> cloudPoints) {
         this.cloudPoints = cloudPoints;
     }
 
